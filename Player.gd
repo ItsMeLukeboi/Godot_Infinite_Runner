8a994cd12b9330e0bb533b3c041129a5d2dba0ae
ItsMeLukeboi <luuklah@gmail.com>
2022-10-07 18:24:08 +0300

    koomenttiii

--- Player.gd
+++ Player.gd
@@ -1,9 +1,10 @@
 extends KinematicBody
 
 var run_speed : float = 8.0
+var sidestep_speed : float = 7.0
+var velocity := Vector3()
 
 var time : float = 0.0
-
 var step_freq : float = 2.0 #kuinka usein
 var step_height : float = 0.2 #kuinka korkea tää hahmo hyppy
 var step_tilt : float = 8.0 #astieta
@@ -24,4 +25,13 @@ func _physics_process(delta):
 	boddy_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time)
 	time += delta
 	
-	move_and_slide(Vector3(0, 0, -run_speed))
+	var sideways: float = 0.0
+	if Input.is_action_pressed("move_right"):
+		sideways += 1.0
+	if Input.is_action_pressed("move_left"):
+		sideways -= 1.0
+		
+	velocity.x = sideways * sidestep_speed
+	velocity.z = -run_speed
+		
+	move_and_slide(Vector3(velocity))
